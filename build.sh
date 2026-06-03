473ee1640b98135b5d449726472e50e9c041de13
pial6215 <pial15-6215@s.diu.edu.bd>
2026-06-03 16:15:51 +0600

    Free admin creation automation

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+DJANGO_SUPERUSER_PASSWORD="Pial4200" python manage.py createsuperuser --username "pial_admin" --email "admin@example.com" --noinput || true
