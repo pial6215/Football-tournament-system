042c48c1360273d6bf0d1b9af986644a64df8d6a
pial6215 <pial15-6215@s.diu.edu.bd>
2026-06-05 21:19:37 +0600

    build.sh path ix

--- build.sh
+++ build.sh
@@ -5,4 +5,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-DJANGO_SUPERUSER_PASSWORD="Pial4200" python manage.py createsuperuser --username "pial_admin" --email "admin@example.com" --noinput || true
