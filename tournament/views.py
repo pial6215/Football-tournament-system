9ee4f9fdb82eef01ec1c0b2ec0d5ef8494a77ae5
pial6215 <pial15-6215@s.diu.edu.bd>
2026-06-09 21:40:07 +0600

    Views code crash fixed permanently

--- tournament/views.py
+++ tournament/views.py
@@ -17,34 +17,36 @@ def home(request):
     
     # Match er data analyze kore table update kora
     for match in matches:
-        if match.is_finished:
+        # 🔥 BUG FIX: match finish holeo score jeno asholeo real number hoy (None jeno na hoy)
+        if match.is_finished and match.team1_score is not None and match.team2_score is not None:
             t1 = match.team1.id
             t2 = match.team2.id
             s1 = match.team1_score
             s2 = match.team2_score
             
             # Match Played count barano
-            team_stats[t1]['mp'] += 1
-            team_stats[t2]['mp'] += 1
-            
-            # Goal Difference calculate kora (Team 1 to Team 2)
-            team_stats[t1]['gd'] += (s1 - s2)
-            team_stats[t2]['gd'] += (s2 - s1)
-            
-            # Win, Loss, Draw mapping
-            if s1 > s2:      # Team 1 jitsi
-                team_stats[t1]['w'] += 1
-                team_stats[t1]['pts'] += 3
-                team_stats[t2]['l'] += 1
-            elif s2 > s1:    # Team 2 jitsi
-                team_stats[t2]['w'] += 1
-                team_stats[t2]['pts'] += 3
-                team_stats[t1]['l'] += 1
-            else:            # Draw hoise
-                team_stats[t1]['d'] += 1
-                team_stats[t2]['d'] += 1
-                team_stats[t1]['pts'] += 1
-                team_stats[t2]['pts'] += 1
+            if t1 in team_stats and t2 in team_stats:
+                team_stats[t1]['mp'] += 1
+                team_stats[t2]['mp'] += 1
+                
+                # Goal Difference calculate kora (Team 1 to Team 2)
+                team_stats[t1]['gd'] += (s1 - s2)
+                team_stats[t2]['gd'] += (s2 - s1)
+                
+                # Win, Loss, Draw mapping
+                if s1 > s2:      # Team 1 jitsi
+                    team_stats[t1]['w'] += 1
+                    team_stats[t1]['pts'] += 3
+                    team_stats[t2]['l'] += 1
+                elif s2 > s1:    # Team 2 jitsi
+                    team_stats[t2]['w'] += 1
+                    team_stats[t2]['pts'] += 3
+                    team_stats[t1]['l'] += 1
+                else:            # Draw hoise
+                    team_stats[t1]['d'] += 1
+                    team_stats[t2]['d'] += 1
+                    team_stats[t1]['pts'] += 1
+                    team_stats[t2]['pts'] += 1
 
     # Points ar GD layout onujayi sorting (Jader points beshi tara upore thakbe)
     sorted_teams = sorted(
